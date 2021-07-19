019ee4c84dcb9bf38f52b2afd6f7c21a938b2e07
d7o1d1s0 <d7sarmy@gmail.com>
2021-07-19 16:22:41 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,10 @@
+'''
+weather guessing game
+'''
+
+weather = input("How is the weather? :")
+
+if weather == 'Awesome!':
+    print("Frankly I couldn't agree more")
+else:
+    print("Yeah.. some would say")
